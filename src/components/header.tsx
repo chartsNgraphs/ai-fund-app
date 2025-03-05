0d0e55908c7aca14b3cac25849e6084f7a2e0144
Dwayne Thomson <dwaynethomson14@gmail.com>
2025-03-04 20:53:05 -0500

    clean up header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,5 @@
+'use server';
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,59 +10,12 @@ import Link from "next/link";
 import { Menu } from "lucide-react";
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { nanoid } from "nanoid";
 import { ModeToggle } from "./dark-mode-toggle";
 import { cookies } from "next/headers";
 import { decrypt } from "@/app/login/session";
 import LoginButtons from "./login-buttons";
 
-const landings = [
-  {
-    id: nanoid(),
-    title: "Landing 01",
-    route: "/project-management",
-  },
-  {
-    id: nanoid(),
-    title: "Landing 02",
-    route: "/crm-landing",
-  },
-  {
-    id: nanoid(),
-    title: "Landing 03",
-    route: "/ai-content-landing",
-  },
-  {
-    id: nanoid(),
-    title: "Landing 04",
-    route: "/new-intro-landing",
-  },
-  {
-    id: nanoid(),
-    title: "Landing 05",
-    route: "/about-us-landing",
-  },
-  {
-    id: nanoid(),
-    title: "Landing 06",
-    route: "/contact-us-landing",
-  },
-  {
-    id: nanoid(),
-    title: "Landing 07",
-    route: "/faqs-landing",
-  },
-  {
-    id: nanoid(),
-    title: "Landing 08",
-    route: "/pricing-landing",
-  },
-  {
-    id: nanoid(),
-    title: "Landing 09",
-    route: "/career-landing",
-  },
-];
+
 
 export default async function Header() {
 
@@ -86,19 +41,6 @@ export default async function Header() {
         <ModeToggle />
         <LoginButtons isLoggedIn={isLoggedIn} />
         <div className="flex md:hidden mr-2 items-center gap-2">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <span className="py-2 px-2 bg-gray-100 rounded-md">Pages</span>
-            </DropdownMenuTrigger>
-
-            <DropdownMenuContent align="start">
-              {landings.map((page) => (
-                <DropdownMenuItem key={page.id}>
-                  <Link href={page.route}>{page.title}</Link>
-                </DropdownMenuItem>
-              ))}
-            </DropdownMenuContent>
-          </DropdownMenu>
 
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
